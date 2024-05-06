290412c336293a858245deed3e596119e6f89f37
Citrodata <39993466+Citrodata@users.noreply.github.com>
2024-05-06 10:09:41 +0200

    Add fuse

--- tools/build_appimage.sh
+++ tools/build_appimage.sh
@@ -47,7 +47,7 @@ if [ -n "$CENTOS" ]; then
 else
     export DEBIAN_FRONTEND=noninteractive
     # ubuntu (>= 18.04) build tools
-    apt-get update && apt-get install -yy git build-essential cmake wget
+    apt-get update && apt-get install -yy git build-essential cmake wget fuse libfuse-dev
     apt-get install -yy zlib1g-dev file libbz2-dev libpng-dev libopus-dev libtheora-dev
     # ubuntu SDL dependencies
     apt-get install -yy libx11-dev libxext-dev libxrandr-dev libxi-dev libxfixes-dev libxcursor-dev
